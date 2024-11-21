8e9e81730f185add7ac52eb28b0bcf25db39f109
Maxim Frolov <max.fro2021@gmail.com>
2024-11-21 23:58:04 +0100

    fix: call `init_es_standard` in `test-rw.c`

--- test/test-rw.c
+++ test/test-rw.c
@@ -1,28 +1,32 @@
-#include "iobuf.h"
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
+#include "iobuf.h"
+
+int main(int argc, char* argv[])
 {
-	FICHIER *f1;
-	FICHIER *f2;
-	char c;
+  FICHIER* f1;
+  FICHIER* f2;
+  char c;
 
-	f1 = ouvrir ("test_files/test_file1.txt", 'L');
-	if (f1 == NULL)
-		exit (-1);
+  init_es_standard();
 
-	f2 = ouvrir ("test_files/test_file2.txt", 'E');
-	if (f2 == NULL)
-		exit (-1);
+  f1 = ouvrir("test_files/test_file1.txt", 'L');
+  if (f1 == NULL)
+    exit(-1);
 
-	while (lire (&c, 1, 1, f1) == 1) {
-          ecrire (&c, 1, 1, stdout);
-          ecrire (&c, 1, 1, f2);
-	}
-/*        vider (stdout);*/
+  f2 = ouvrir("test_files/test_file2.txt", 'E');
+  if (f2 == NULL)
+    exit(-1);
 
-        fermer (f1);
-        fermer (f2);
+  while (lire(&c, 1, 1, f1) == 1) {
+    ecriref("%c", c);
+    ecrire(&c, 1, 1, f2);
+  }
+  ecriref("\n");
+  vider(stdout);
 
-	return 0;
+  fermer(f1);
+  fermer(f2);
+
+  return 0;
 }
